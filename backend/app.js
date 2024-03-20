e0a70ef03e040c5e3dfb117b97e0ae42343e5a37
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2024-03-20 14:31:09 +0530

    add files for backend deployment

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,10 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
+});
+
 app.get("/api/v1/user", getUser);
 
 module.exports = app;
